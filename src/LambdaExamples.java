dc746df1da9f55e0e30110dced8094df9a51d217
Mark <SwiftTactics@gmail.com>
2021-04-25 03:46:17 -0400

    set up a basic lambda, added an employee class

--- src/LambdaExamples.java
+++ src/LambdaExamples.java
@@ -1,8 +1,58 @@
-public class LambdaExamples implements Runnable {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaExamples{
+
+    public static void main(String[] args) {
+        // old school way.
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Printing from the runnable");
+//            }
+//        }).start();
+
+        // lambda way!
+        // 3 parts -- Argument list, Arrow Token, Body.
+        // in this case the argument list is empty ()
+        // the arrow token is ->
+        // the body is the code we are running Sys.out
+        // This knows what to do because Run is the only method! (functional interface)
+        new Thread(()-> System.out.println("Printing from the Runnable")).start();
+
+        // can add a code block to add to lambda
+        new Thread(()-> {
+            System.out.println("Printing from the Runnable");
+            System.out.println("Line 2");
+            System.out.format("This is line %d\n", 3);
+        }).start();
+
+        Employee john = new Employee("John Joe", 30);
+        Employee mark = new Employee("Mark Marcello", 31);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
 
 
-    @Override
-    public void run() {
-        System.out.println("Printing from the runnable");
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(mark);
+        employees.add(jack);
+        employees.add(snow);
+
+        // comparator is a functional interface because it has one method that needs an override
+        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee employee1, Employee employee2) {
+                return employee1.getName().compareTo(employee2.getName());
+            }
+        });
+
+        for(Employee employee: employees){
+            System.out.println(employee.getName());
+        }
     }
+
+
 }
