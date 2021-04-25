cc74b2759b6bfef8ed00cba6dd3610e1a059b705
Mark <SwiftTactics@gmail.com>
2021-04-25 04:02:11 -0400

    added a interface for uppercase and concatination

--- src/LambdaExamples.java
+++ src/LambdaExamples.java
@@ -64,7 +64,21 @@ public class LambdaExamples{
         for(Employee employee: employees){
             System.out.println(employee.getName());
         }
+
+
+        String sillyString = doStringStuff(new UpperConcat() {
+            @Override
+            public String upperAndConcat(String s1, String s2) {
+                return s1.toUpperCase() + s2.toUpperCase();
+            }
+        },
+        employees.get(0).getName(), employees.get(1).getName());
+        System.out.println(sillyString);
+
     }
 
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2){
+        return uc.upperAndConcat(s1, s2);
+    }
 
 }
---
+++ src/UpperConcat.java
@@ -0,0 +1,3 @@
+public interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
+}
